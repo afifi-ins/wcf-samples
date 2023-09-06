00e73322b83ee2b8b75d44bda9fa5518eb2db593
Abdelwahab Afifi <v-aafifi@microsoft.com>
2023-09-06 08:36:32 +0300

    add constructor

---
+++ framework/wcf/Basic/Binding/Basic/MessageSecurity/CS/client/CalculatorClientConstructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Microsoft.Samples.MessageSecurity
+{
+    // This partial class constructor is needed as the service is hosted in IIS and advertises both HTTP and HTTPS endpoints.
+    // This causes the generated client to have 2 configurations so a default constructor can't be generated as it's unknown which to use.
+    // By adding this constructor, we can choose which endpoint we wish to use as the default.
+    public partial class CalculatorClient
+    {
+#if NET6_0_OR_GREATER
+        //public CalculatorClient() : this(EndpointConfiguration.BasicHttpBinding_ICalculator1) { }
+#endif
+    }
+}
